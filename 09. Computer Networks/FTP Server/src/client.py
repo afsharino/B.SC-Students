7f16555f67653e298eca49c5e695b062f07f1369
afsharino@archiso <mohammadafshari.fum.ac.ir@gmial.com>
2023-06-29 16:18:54 +0330

    implement connect to server method in client side.

--- 09. Computer Networks/FTP Server/src/client.py
+++ 09. Computer Networks/FTP Server/src/client.py
@@ -9,7 +9,26 @@ class Client():
         self.SERVER_PORT = 9090
         self.BUFFER_SIZE = 4096
 
+        
+
+    
+    def connect_to_server(self):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(f'Trying to connect to the server...')
+        client.connect((self.SERVER_HOST, self.SERVER_PORT))
+
+        while True:
+            server_response = client.recv(self.BUFFER_SIZE).decode('utf-8')
+            status, message = server_response.split('@')
+
+            if status == 'OK':
+                print(message)
+            
+            elif status == "ERROR":
+                print(message)
+
 
 if __name__ == '__main__':
     
-    server = Client()
+    client = Client()
+    client.connect_to_server()
